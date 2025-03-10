924a9a5f1fdc19a05c7f34492b93f926a5fc5a18
P3droSoares <pedro.soares.contato@hotmail.com>
2025-03-10 19:38:50 -0300

    base

--- tests/api/v1/status/status.test.js
+++ tests/api/v1/status/status.test.js
@@ -4,4 +4,7 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const responseBody = await response.json();
   expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
 });
